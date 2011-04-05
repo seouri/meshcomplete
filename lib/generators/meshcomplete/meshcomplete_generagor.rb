800d21e317315f87605615cd6dededc2fb4b153d
Joon Lee <seouri@gmail.com>
2011-04-05 12:04:38 -0400

    add next migration number

--- lib/generators/meshcomplete/meshcomplete_generagor.rb
+++ lib/generators/meshcomplete/meshcomplete_generagor.rb
@@ -8,6 +8,14 @@ class MeshcompleteGenerator < Rails::Generators::Base
     @source_root ||= File.join(File.dirname(__FILE__), 'templates')
   end
 
+  def self.next_migration_number(dirname)
+    if ActiveRecord::Base.timestamped_migrations
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
+    else
+      "%.3d" % (current_migration_number(dirname) + 1)
+    end
+  end
+
   def create_migration_file
     migration_template 'migration.rb', 'db/migrate/create_synonyms.rb'
   end

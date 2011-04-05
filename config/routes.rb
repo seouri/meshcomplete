4161fd9efbbbbe3db6e1cf6a8eee86c8f48278a0
Joon Lee <seouri@gmail.com>
2011-04-05 11:03:41 -0400

    fix route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   mount_at = Meshcomplete::Engine.config.mount_at
-  match mount_at + meshcomplete => "meshcomplete#search"
+  match mount_at + "meshcomplete" => "meshcomplete#search"
 end

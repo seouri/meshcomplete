e4d4c94eab2378baf5de88a43203a22d2c06247b
Joon Lee <seouri@gmail.com>
2011-04-12 02:25:36 -0400

    refactor view

--- lib/meshcomplete/action_view_extension.rb
+++ lib/meshcomplete/action_view_extension.rb
@@ -3,7 +3,7 @@ module Meshcomplete
     extend ActiveSupport::Concern
     module InstanceMethods
       def meshcomplete
-        '<div id="meshcomplete-box"></div>'.html_safe
+        '<div id="meshcomplete-box"><input id="meshcomplete" /><span id="meshcomplete-clear" title="Clear search">&times;</span><input type="hidden" id="meshcomplete-selected-ids"/></div><div id="meshcomplete-update"></div>'.html_safe
       end
     end
   end

1a75b18d4d50789ca13e173789470073b42cc6b0
Joon Lee <seouri@gmail.com>
2011-04-13 00:49:13 -0400

    maintain state

--- lib/meshcomplete/action_view_extension.rb
+++ lib/meshcomplete/action_view_extension.rb
@@ -2,8 +2,8 @@ module Meshcomplete
   module ActionViewExtension
     extend ActiveSupport::Concern
     module InstanceMethods
-      def meshcomplete
-        '<div id="meshcomplete-box"><input id="meshcomplete" /><span id="meshcomplete-clear" title="Clear search">&times;</span><input type="hidden" id="meshcomplete-selected-ids"/></div><div id="meshcomplete-update"></div>'.html_safe
+      def meshcomplete(ids)
+        "<div id=\"meshcomplete-box\"><input id=\"meshcomplete\" /><span id=\"meshcomplete-clear\" title=\"Clear search\">&times;</span><input type=\"hidden\" id=\"meshcomplete-selected-ids\" value=\"#{ids}\"/></div><div id=\"meshcomplete-update\"></div>".html_safe
       end
     end
   end

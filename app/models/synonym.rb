22fbf24fe992c3fd54cc4306325fd75e286ef286
Joon Lee <seouri@gmail.com>
2011-05-19 16:55:15 -0400

    compatibility for postgresql

--- app/models/synonym.rb
+++ app/models/synonym.rb
@@ -2,7 +2,8 @@ class Synonym < ActiveRecord::Base
   belongs_to :subject
   
   def self.search(starts_with, limit = 10)
-    mesh = where(["term LIKE ?", "#{starts_with}%"]).limit(limit).order("term asc").includes(:subject).group_by(&:subject)
+    match_keyword = ::ActiveRecord::Base.connection.adapter_name == "PostgreSQL" ? "ILIKE" : "LIKE"
+    mesh = where(["term #{match_keyword} ?", "#{starts_with}%"]).limit(limit).order("term asc").includes(:subject).group_by(&:subject)
     mesh.keys.compact.sort_by(&:term).map {|m| {"id" => m.id, "label" => m.term, "value" => m.term, "entries" => mesh[m].reject {|e| m.term == e.term}.map{|e| e.term}.sort}}
   end
 end
